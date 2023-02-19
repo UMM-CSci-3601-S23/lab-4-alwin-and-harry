e39d2662370ebb56ea6cd461929ea8d8228bd3e5
Alwin Lor <lor00148@morris.umn.edu>
2023-02-19 13:32:54 -0600

    add angular filters
    add filters for owner, body, and category
    
    Co-authored-by: Harry <lv000013@morris.umn.edu>

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Todo, TodoCategory } from './todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  readonly todoUrl: string = environment.apiUrl + 'todos';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+  //Filters here
+
+  //Filtering using the database
+
+  //Filtering using Angular
+  //Body, owner, and, category are being filtered through Angular
+
+  filterTodos(todos: Todo[], filters?: { body?: string; category?: string; owner?: string }): Todo[] {
+    let filteredTodos = todos;
+
+    if (filters.body){
+      filters.body = filters.body.toLowerCase();
+      filteredTodos = filteredTodos.filter(todo => todo.body.toLowerCase().indexOf(filters.body) !== -1);
+    }
+
+    if (filters.category){
+      filters.category = filters.category.toLowerCase();
+      filteredTodos = filteredTodos.filter(todo => todo.category.toLowerCase().indexOf(filters.category) !== -1);
+    }
+    if (filters.owner){
+      filters.owner = filters.owner.toLowerCase();
+      filteredTodos = filteredTodos.filter(todo => todo.owner.toLowerCase().indexOf(filters.owner) !== -1);
+    }
+
+    return filteredTodos;
+
+}
 }
