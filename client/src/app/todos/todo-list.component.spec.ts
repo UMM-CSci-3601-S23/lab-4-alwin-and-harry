b234ae45317612649564269ce51132f6644cf5f2
Alwin Lor <lor00148@morris.umn.edu>
2023-02-22 18:46:28 -0600

    create test todo-list.component
    create a test for getTodoFromServer testing the status

--- client/src/app/todos/todo-list.component.spec.ts
+++ client/src/app/todos/todo-list.component.spec.ts
@@ -1,23 +1,72 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { MockUserService } from '../../testing/user.service.mock';
+import { Todo } from './todo';
 import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MockTodoService } from 'src/testing/todo.service.mock';
+
+const COMMON_IMPORTS: any[] = [
+  FormsModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatListModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatIconModule,
+  MatSnackBarModule,
+  BrowserAnimationsModule,
+  RouterTestingModule,
+];
 
 describe('TodoListComponent', () => {
-  let component: TodoListComponent;
+  let todoList: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(TodoListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [COMMON_IMPORTS],
+      declarations: [ TodoListComponent ],
+      providers: [{provide: TodoService, useValue: new MockTodoService()}]
+    });
   });
+  beforeEach(waitForAsync(() => {
+    TestBed.compileComponents().then(() => {
+    fixture = TestBed.createComponent(TodoListComponent);
+    todoList = fixture.componentInstance;
+    fixture.detectChanges();
+    });
+  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should contain all todos', () => {
+    expect(todoList.serverFilteredTodo.length).toBe(4);
+  });
+  it('should contain todos with status \'true\'', () => {
+    expect(todoList.serverFilteredTodo.some((todo: Todo) => todo.status === true)).toBe(true);
+  });
+  it('should contain todos with status \'false\'', () => {
+    expect(todoList.serverFilteredTodo.some((todo: Todo) => todo.status === false)).toBe(true);
   });
 });
