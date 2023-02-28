92a13084cdf1b4b7b297baab2a27f47236bb0f68
Harry <lv000013@morris.umn.edu>
2023-02-27 19:07:16 -0600

    Modified some limitTodos

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -19,6 +19,7 @@ export class TodoListComponent implements OnInit{
   public todoStatus: boolean;
   public todoBody: string;
   public todoCategory: TodoCategory;
+  public todoLimit: number;
   public viewType: 'card' | 'list' = 'card';
 
   private ngUnsubscribe = new Subject<void>();
@@ -51,7 +52,7 @@ export class TodoListComponent implements OnInit{
 
   public updateFilter(){
     this.filteredTodos = this.todoService.filterTodos(
-      this.serverFilteredTodo, { body: this.todoBody, category: this.todoCategory, owner: this.todoOwner }
+      this.serverFilteredTodo, { body: this.todoBody, category: this.todoCategory, owner: this.todoOwner, limit: this.todoLimit }
     );
   }
   ngOnInit(): void {
